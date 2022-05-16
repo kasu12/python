62c4b3dfd76984ffe35f1ed066999620caf16c0a
Kasahun Alemayehu <kasu121921@gmail.com>
2022-05-16 23:37:16 +0300

    first chapter first question

---
+++ chapter_1/exercise1-1-1.py
@@ -0,0 +1,14 @@
+from asyncore import write
+import stdio
+
+stdio.writeln("hello world")
+stdio.writeln("hello world")
+stdio.writeln("hello world")
+stdio.writeln("hello world")
+stdio.writeln("hello world")
+stdio.writeln("hello world")
+stdio.writeln("hello world")
+stdio.writeln("hello world")
+stdio.writeln("hello world")
+stdio.writeln("hello world")
+stdio.writeln("hello world")
