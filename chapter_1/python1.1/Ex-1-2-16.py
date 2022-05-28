99cda41b3b411bf0d3abe9ef44557906b83e038b
Kasahun Alemayehu <kasu121921@gmail.com>
2022-05-28 23:27:16 +0300

    Ex-1-2-16 solution

---
+++ chapter_1/python1.1/Ex-1-2-16.py
@@ -0,0 +1,9 @@
+import stdio
+import random
+
+num1 = input(" please enter the first number")
+num2 = input(" please enter the second number")
+
+stdio.writeln(num1)
+stdio.writeln(random.random())
+stdio.writeln(num2)
