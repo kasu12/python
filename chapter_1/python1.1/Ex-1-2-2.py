ee0dbd23a196f05906505287efdcb7ec5e360b6c
Kasahun Alemayehu <kasu121921@gmail.com>
2022-05-28 23:13:26 +0300

    Ex-1-2-2 solution

---
+++ chapter_1/python1.1/Ex-1-2-2.py
@@ -0,0 +1,12 @@
+from cmath import cos
+import math
+import stdio
+
+
+x = float(input("enter value for teta "))
+
+val = (math.cos(x))**2 + (math.sin(x))**2
+
+stdio.writeln(val)
+
+# because the value will always rounded
